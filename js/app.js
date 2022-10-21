3edfc4f0de1d81a4fdd0daf2e652dac2791a864b
rodriquelali <rodriquelali@gmail.com>
2022-10-21 16:30:22 -0400

    peticion https, a jsonplaholder.

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const httpRequest = ()=>{
+    //consumiendo api de jsonpleholder
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response=>response.json())
+    .then(json=>{
+        console.log(json);
+        let data = "";
+        json.forEach(element => {
+            data += `<div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-01.jpg" alt="..." /></div>
+            <div class="col-lg-6">
+                <div class="bg-black text-center h-100 project">
+                    <div class="d-flex h-100">
+                        <div class="project-text w-100 my-auto text-center text-lg-left">
+                            <h4 class="text-white">${element.title}</h4>
+                            <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                            <hr class="d-none d-lg-block mb-0 ms-0" />
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+            console.log(element.title);
+        });
+        document.querySelector('.cuerpo').innerHTML = data;
+    })
+
+}
+httpRequest();
